9d6fc9dbfefc8580824efb6905e66b13e98d6c24
keedow <astelea3@gmail.com>
2022-09-07 14:38:07 +0300

    :)

--- ProiectASP/ProiectASP/Controllers/AngajatController.cs
+++ ProiectASP/ProiectASP/Controllers/AngajatController.cs
@@ -22,36 +22,50 @@ namespace ProiectASP.Controllers
         }
 
         [HttpGet("GetAllAngajati")]
-        public List<Angajat> GetAllAngajati([FromQuery]int id)
+        public List<Angajat> GetAllAngajati([FromQuery] int id)
         {
             return _context.Angajats.Select(a => new Angajat() { Id = a.Id, Nume = a.Nume, Prenume = a.Prenume, Email = a.Email, Parola = a.Parola, DataAngajare = a.DataAngajare, DataNasterii = a.DataNasterii, Cnp = a.Cnp, Serie = a.Serie, No = a.No, NrTelefon = a.NrTelefon, EsteAdmin = a.EsteAdmin, ManagerId = a.ManagerId, DepartamentId = a.DepartamentId, FunctieId = a.FunctieId }).ToList();
         }
 
         [HttpGet("GetAngajatByUsername")]
 
-        public Angajat GetAllAngajatByUsername([FromQuery]string username, [FromQuery] string parola)
+        public Angajat GetAllAngajatByUsername([FromQuery] string username, [FromQuery] string parola)
         {
-            Angajat a =  _context.Angajats.Select(a => a).Where(a => a.Email.Equals(username)).FirstOrDefault();
+            Angajat a = _context.Angajats.Select(a => a).Where(a => a.Email.Equals(username)).FirstOrDefault();
             if (a != null)
             {
-                if(a.Parola.Equals(parola))
+                if (a.Parola.Equals(parola))
                 {
                     return a;
                 }
-              
+
             }
             return null;
         }
         [HttpGet("GetAllManagers")]
         public List<Angajat> GetAllManagers()
         {
-            return _context.Angajats.Include(a =>a.Manager).Select(a=>a).Where(a=>a.ManagerId==26).Where(a=>a.Id!=26).ToList();
+            return _context.Angajats.Include(a => a.Manager).Select(a => a).Where(a => a.ManagerId == 26).Where(a => a.Id != 26).ToList();
         }
         [HttpGet("GetManagersAngajat")]
-        public List<Angajat> GetManagersAngajat([FromQuery]int idManag)
+        public List<Angajat> GetManagersAngajat([FromQuery] int idManag)
         {
+
             return _context.Angajats.Include(a => a.Manager).Select(a => a).Where(a => a.ManagerId == idManag).Where(a => a.Id != idManag).ToList();
         }
+
+        [HttpPut("PutNewAngajat")]
+
+        public  Angajat AdaugaAngajat([FromBody]Angajat ang)
+        {
+             _context.Angajats.Add(ang);
+            _context.SaveChanges();
+
+            return ang;
+        }
+    
+
   
     }
+    
 }
