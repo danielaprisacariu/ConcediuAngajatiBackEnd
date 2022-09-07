2f04b1306cd943c878e94d469db6f2d252b55909
Damian Mares <damian.mares@totalsoft.ro>
2022-09-07 16:04:04 +0300

    nghg

--- ProiectASP/ProiectASP/Controllers/ConcediuController.cs
+++ ProiectASP/ProiectASP/Controllers/ConcediuController.cs
@@ -40,6 +40,24 @@ namespace ProiectASP.Controllers
 
         }
 
+        [HttpGet("GetAllConcediuManager")]
+        public List<Concediu> GetAllConcediuManager()
+        {
+            return _context.Concedius
+                .Include(c => c.Angajat.Manager)
+                .Include(c => c.Inlocuitor)
+                .Include(c => c.StareConcediu)
+                .Select(c => new Concediu(c.Id, c.DataInceput, c.DataSfarsit, c.Comentarii
+                , new Angajat { Id = c.Angajat.Id, Nume = c.Angajat.Nume, Prenume = c.Angajat.Prenume }
+                , new Angajat { Id = c.Inlocuitor.Id, Nume = c.Inlocuitor.Nume, Prenume = c.Inlocuitor.Prenume }
+                , new TipConcediu { Nume = c.TipConcediu.Nume }
+                , new StareConcediu { Id = c.StareConcediu.Id, Nume = c.StareConcediu.Nume }
+                )).ToList();
+
+            //_context.Concedius.Include(c => c.Angajat.Manager).Include(c => c.StareConcediu).Select(c => c).ToList();
+
+        }
+
         [HttpPut("PutConcediu")]
         public bool UpdateStareCncediu([FromQuery]int idConcediu, [FromQuery] int idStareConcediu)
         {
